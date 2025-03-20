d362be9a05334b72b7c04e34ae1f8834a351573b
yulin0625 <yulin90625@gmail.com>
2025-03-21 03:28:30 +0800

    Add source code and docs for Lab 1

---
+++ lab_1/src/Debug/FreeRTOS/queue.d
@@ -0,0 +1,23 @@
+FreeRTOS/queue.o: ../FreeRTOS/queue.c \
+ D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/FreeRTOS.h \
+ D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/FreeRTOSConfig.h \
+ D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/projdefs.h \
+ D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/portable.h \
+ D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/deprecated_definitions.h \
+ D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/mpu_wrappers.h \
+ D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/task.h \
+ D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/list.h \
+ D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/queue.h \
+ D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/task.h
+D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/FreeRTOS.h:
+D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/FreeRTOSConfig.h:
+D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/projdefs.h:
+D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/portable.h:
+D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/deprecated_definitions.h:
+D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/mpu_wrappers.h:
+D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/task.h:
+D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/list.h:
+D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/queue.h:
+D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/task.h:
---
+++ lab_1/src/Debug/FreeRTOS/stream_buffer.d
@@ -0,0 +1,21 @@
+FreeRTOS/stream_buffer.o: ../FreeRTOS/stream_buffer.c \
+ D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/FreeRTOS.h \
+ D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/FreeRTOSConfig.h \
+ D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/projdefs.h \
+ D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/portable.h \
+ D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/deprecated_definitions.h \
+ D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/mpu_wrappers.h \
+ D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/task.h \
+ D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/list.h \
+ D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/stream_buffer.h
+D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/FreeRTOS.h:
+D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/FreeRTOSConfig.h:
+D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/projdefs.h:
+D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/portable.h:
+D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/deprecated_definitions.h:
+D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/mpu_wrappers.h:
+D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/task.h:
+D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/list.h:
+D:/SynologyDrive/STM32CubeIDE/workspace_1.17.0/lab_1_new/FreeRTOS/include/stream_buffer.h:
